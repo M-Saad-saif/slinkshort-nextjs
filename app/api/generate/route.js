fe10d3f291601943a07169f8689ecc52fdae4d37
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-02-08 01:56:18 +0500

    route working, fetching the generate

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,11 +1,22 @@
 import clientPromise from "@/app/lib/moongoDB";
 
 export async function POST(request) {
-  const body =  await request.json();
+  const body = await request.json();
 
   const client = await clientPromise;
   const db = client.db("urlshorten");
   const collection = db.collection("urls");
+
+  // checking if the short URL is already taken
+  const existingShortUrl = await collection.findOne({
+    shorturl: body.shorturl,
+  });
+  if (existingShortUrl) {
+    return Response.json({
+      success: false,
+      message: "URL already exist",
+    });
+  }
 
   const result = await collection.insertOne({
     url: body.url,
--- app/generate/page.js
+++ app/generate/page.js
@@ -1,12 +1,31 @@
 "use client";
 import React, { useState } from "react";
+import Link from "next/link";
 import "../style.css";
 
 export default function page() {
   const [url, setUrl] = useState("");
   const [shorturl, setShorturl] = useState("");
-  const [generate, setGenerate] = useState(false);
+  const [generate, setGenerate] = useState("");
 
+  const GenerateURL = async () => {
+    // fteching
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: url, shorturl: shorturl }),
+    });
+
+    const data = await response.json();
+    if (data) {
+      alert(data.message);
+      setGenerate(`http://localhost:3000/${shorturl}`);
+      setUrl("");
+      setShorturl("");
+    }
+  };
 
   return (
     <section className="generate-container justify-items-center mt-30">
@@ -31,9 +50,23 @@ export default function page() {
           value={shorturl}
         />
 
-        <button className="px-9 z-30 py-2 bg-[#00916f] rounded-md text-white relative font-semibold  overflow-hidden  after:absolute after:h-1 after:w-1 after:bg-[#00d9a6] after:left-5 after:bottom-0  after:-z-20 after:translate-y-full after:rounded-md hover:after:scale-[300]  after:transition-all after:duration-700 hover:after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#00d9a6] hover:[text-shadow:3px_5px_2px_#00916f] text-[18px] mt-6  w-116">
+        <button
+          onClick={GenerateURL}
+          className="px-9 z-30 py-2 bg-[#00916f] rounded-md text-white relative font-semibold  overflow-hidden  after:absolute after:h-1 after:w-1 after:bg-[#00d9a6] after:left-5 after:bottom-0  after:-z-20 after:translate-y-full after:rounded-md hover:after:scale-[300]  after:transition-all after:duration-700 hover:after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#00d9a6] hover:[text-shadow:3px_5px_2px_#00916f] text-[18px] mt-6  w-116"
+        >
           Generate
         </button>
+
+        <div className="shorturl">
+          {generate && (
+            <>
+              <h2 className="text-[19px] font-bold">Your Link</h2>
+              <Link href={generate} target="_blank">
+                <code>{generate}</code>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </section>
   );
